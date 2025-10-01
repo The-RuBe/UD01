3fb47ff233c5e6325a8f98237f687d5848c40b9a
The-RuBe <rubenroldanjimenez12@gmail.com>
2025-10-01 14:42:22 +0200

    01/10 no me he enterado de nada

---
+++ prueba/src/prueba/EjemploPersona.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class EjemploPersona {
+	
+	String nombre;
+	int edad;
+	double estatura;
+	
+	void presentarse() {
+	System.out.printf("Mi nombre es %s, tengo es %d años y mido es %.2f m" ,nombre,edad,estatura);
+
+	}
+
+}
---
+++ prueba/src/prueba/PruebaClase.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class PruebaClase {
+
+	public static void main(String[] args) {
+		
+		EjemploPersona p1 = new EjemploPersona();
+		
+		p1.nombre = "Rubén";
+		p1.edad = 19;
+		p1.estatura = 1.71;
+		p1.presentarse();								
+
+	}
+
+}
