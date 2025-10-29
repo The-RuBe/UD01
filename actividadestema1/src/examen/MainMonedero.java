35ccd22d7676fc63fbf41dce0043fe87f30a1056
The-RuBe <rubenroldanjimenez12@gmail.com>
2025-10-29 12:48:35 +0100

    cambiosExamen

--- actividadestema1/src/examen/MainMonedero.java
+++ actividadestema1/src/examen/MainMonedero.java
@@ -2,37 +2,36 @@ package examen;
 import java.util.Scanner;
 
 public class MainMonedero {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 
-System.out.println("Introduce los billetes de 5 euros: ");
+	System.out.println("Introduce los billetes de 5 euros: ");
 
-int cincoEuros = sc.nextInt();
+		int cincoEuros = sc.nextInt();
+	
+	System.out.println("Introduce las monedas de 2 euros: ");
+	
+		int dosEuros = sc.nextInt();
+	
+	System.out.println("Introduce las monedas de 1 euro: ");
+	
+		int unEuro = sc.nextInt();
+	
+	System.out.println("Introduce las monedas de 50 céntimos: ");
+	
+		int cent50 = sc.nextInt();
+	
+	System.out.println("Introduce las monedas de 20 céntimos: ");
+	
+		int cent20 = sc.nextInt();
+	
+	Monedero m = new Monedero(cincoEuros, dosEuros, unEuro, cent50, cent20);
+	
+		m.calcularTotalEuros();
+	
+	sc.close();
 
-System.out.println("Introduce las monedas de 2 euros: ");
-
-
-int dosEuros = sc.nextInt();
-
-System.out.println("Introduce las monedas de 1 euro: ");
-
-int unEuro = sc.nextInt();
-
-System.out.println("Introduce las monedas de 50 céntimos: ");
-
-int cent50 = sc.nextInt();
-
-System.out.println("Introduce las monedas de 20 céntimos: ");
-
-int cent20 = sc.nextInt();
-
-Monedero m = new Monedero(cincoEuros, dosEuros, unEuro, cent50, cent20);
-
-m.calcularTotalEuros();
-
-sc.close();
-
-}
+	}
 
 }
